a3f55c3e41200f9836edcd4a33a679033b81dbdf
Braham Prakash <developerbrm@gmail.com>
2023-11-21 13:48:06 +0530

    - tailwind config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './utilities/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
