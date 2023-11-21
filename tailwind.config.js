9947c91dd081fcd7027b962e601ef4fb7f7dd5b4
Braham Prakash <developerbrm@gmail.com>
2023-11-21 13:46:24 +0530

    - icons added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: ['light'],
+  },
+  plugins: [require('daisyui')],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -15,4 +19,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
