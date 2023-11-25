c8d8391e3585a1304428bd3e2951fead2ed35efa
Braham Prakash <developerbrm@gmail.com>
2023-11-26 01:34:22 +0530

    - Our works section done

---
+++ components/OurWorks.js
@@ -0,0 +1,105 @@
+import Image from 'next/image'
+import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
+import img1 from '../public/assets/images/img-1.png'
+
+const OurWorks = () => {
+  const imagesArr = [...Array(18)].map((_, index) => ({
+    src: `/assets/images/img-${index + 1}.png`,
+  }))
+
+  const imageWidth = '10rem'
+
+  return (
+    <div className="">
+      <div className="my-5 px-4 mx-auto max-w-7xl">
+        <div className="my-5 text-center text-3xl font-bold text-amber-500">
+          Our Works
+        </div>
+
+        <div
+          className={`grid grid-cols-2 justify-between gap-x-3 gap-y-8 md:grid-cols-3 md:gap-x-6 lg:grid-cols-4 lg:gap-x-6`}
+          style={
+            {
+              // gridTemplateColumns: `repeat(auto-fit, minmax(${imageWidth}, 1fr))`,
+            }
+          }
+        >
+          {imagesArr.map((obj, index) => {
+            const { src, alt = '' } = obj
+
+            let isBooked, isAvailable
+
+            isAvailable = Math.random() * 10 > 5
+
+            isBooked = !isAvailable
+
+            return (
+              <>
+                <div className="card w-full bg-white text-slate-900 shadow-xl">
+                  <figure
+                    className="relative aspect-square"
+                    key={uuidv4()}
+                    style={
+                      {
+                        // width: imageWidth,
+                      }
+                    }
+                  >
+                    <Image
+                      src={src}
+                      alt={alt}
+                      fill={true}
+                      className="apply-base-img-css object-cover"
+                    />
+                  </figure>
+                  <div className="card-body p-4">
+                    <div className="flex gap-1">
+                      {/* <div className="badge bg-slate-900 text-white">
+                        Backlit
+                      </div> */}
+                      {isAvailable && (
+                        <div className="badge bg-emerald-700 text-white">
+                          Available
+                        </div>
+                      )}
+                      {isBooked && (
+                        <div className="badge bg-rose-700 text-white">
+                          Booked
+                        </div>
+                      )}
+                    </div>
+                    <div className="space-y-1 text-xs lg:text-sm">
+                      <div className="flex gap-1">
+                        <div className="font-bold">Size:</div>
+                        <div className="font-normal">10x6</div>
+                      </div>
+                      <div className="flex gap-1">
+                        <div className="font-bold">Illumination:</div>
+                        <div className="font-normal">Backlit</div>
+                      </div>
+                      <div className="flex gap-1">
+                        <div className="font-bold">Media:</div>
+                        <div className="font-normal">Unipole</div>
+                      </div>
+                      <div className="flex gap-1">
+                        <div className="font-bold">Location:</div>
+                        <address className="link line-clamp-1">
+                          Outside Ritz Banquet, Opposite DLF Capital Greens and
+                          DLF One Midtown, Moti Nagar
+                        </address>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default OurWorks
