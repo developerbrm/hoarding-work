f8ae2a12d29f6e633702c07b1d07f246494523d4
developerbrm <developerbrm@gmail.com>
2024-02-29 10:45:43 +0530

    - hidden class added in the html tag

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="hidden">
       <head>
         <link
           rel="apple-touch-icon"
